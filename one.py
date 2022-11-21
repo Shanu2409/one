738242cf396f016d35b69793300424aa14501931
shanu <shanu@pop-os.localdomain>
2022-11-21 06:04:14 +0530

    first

---
+++ one.py
@@ -0,0 +1,2 @@
+import math as m
+print(m.floor(42.25))
---
+++ some.java
@@ -0,0 +1,9 @@
+class HelloWorld
+{
+	public static void main(String args[])
+	{
+		int num = 124;
+		System.out.println("Hello, World");
+		System.out.println("The number is " + num);
+	}
+}
